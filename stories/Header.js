c16af3db6d61800063ddc58bd94ecaa55354bb5e
akruti-addweb <akruti@addwebsolution.in>
2023-03-30 12:50:32 +0530

    CHanges done on header component

--- stories/Header.js
+++ stories/Header.js
@@ -8,7 +8,7 @@ export const createHeader = ({ user, onLogout, onLogin, onCreateAccount }) => {
   wrapper.className = 'wrapper';
 
   const logo = `<div>
-    <h1>AddWebSolution</h1>
+    <h1>AddWeb</h1>
   </div>`;
 
   wrapper.insertAdjacentHTML('afterbegin', logo);
